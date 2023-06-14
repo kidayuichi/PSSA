6e3178689842e1b651b62572f0b74e4aff53d67c
kidayuichi <114062635+kidayuichi@users.noreply.github.com>
2023-06-14 18:20:43 +0900

    Update 20230607_1_poke.js

--- db/migrations/20230607_1_poke.js
+++ db/migrations/20230607_1_poke.js
@@ -32,23 +32,4 @@ exports.up = function (knex) {
  */
 exports.down = function (knex) {
   return knex.schema.dropTable("poke");
-  table.increments("id").primary(); // Set this column as the primary key
-  table.integer("cardID");
-  table.string("name", 32);
-  table.string("cardURL", 128);
-  table.string("Attack1name", 32);
-  table.string("Attack1damage", 32);
-  table.string("Attack1energy1", 32);
-  table.string("Attack1energy2", 32);
-  table.string("Attack1energy3", 32);
-  table.string("Attack1energy4", 32);
-  table.string("Attack1energy5", 32);
-  table.string("Attack2name", 32);
-  table.string("Attack2damage", 32);
-  table.string("Attack2energy1", 32);
-  table.string("Attack2energy2", 32);
-  table.string("Attack2energy3", 32);
-  table.string("Attack2energy4", 32);
-  table.string("Attack2energy5", 32);
-  table.string("cardID2", 32).primary();
 };
